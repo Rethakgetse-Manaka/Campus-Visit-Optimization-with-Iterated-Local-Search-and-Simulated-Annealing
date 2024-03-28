2070659fb23782f373e898f7602249d6e78b69ba
Rethakgetse-Manaka <rethakgetse11@gmail.com>
2024-03-28 14:49:33 +0200

    Testing Cost_Matrix

--- Main.java
+++ Main.java
@@ -1,3 +1,14 @@
 public class Main {
-
+    public static void main(String[] args) {
+        // Create a cost matrix
+        Cost_Matrix costMatrix = null;
+        // IterativeSearch ILS = null;
+        try {
+            costMatrix = new Cost_Matrix("Cost_Matrix.txt", 5, 5);
+        } catch (Exception e) {
+            System.out.println("File not found");
+            System.exit(1);
+        }
+       System.out.println(costMatrix);
+    }
 }
